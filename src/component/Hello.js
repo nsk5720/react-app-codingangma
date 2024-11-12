310c77a1d4df17b8d8f387d55023a4b8259b02b1
nsk5720 <nsgk5720@gmail.com>
2024-11-12 21:47:53 +0900

    props

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,20 +1,23 @@
 import World from './World'
 import styles from "./Hello.module.css"
 import { useState } from 'react';
+import UserName from './UserName'
 
-export default function Hello() {
+export default function Hello({age}) {
     // let name = "Mike";
     const [name, setName] = useState('Mike');
+    const msg = age > 19 ? "성인입니다." : "미성년자입니다."
     function changeName() {
-
         //const newName = name === "Mike" ? "jane" : "Mike"
         console.log(name)
         setName(name === "Mike" ? "jane" : "Mike")
         // document.getElementById("name").innerText = name;
     }
+
     return (
         <div>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">{name}({age}) {msg}</h2>
+            <UserName name={name}/>
             <button onClick={changeName} >change</button>
         </div>
     );
---
+++ src/component/UserName.js
@@ -0,0 +1,3 @@
+export default function Username({name}) {
+    return <p>Hello, {name}</p>
+}
