6de2dbb47321b51daf36b7d518e4025c2cc87cdd
nsk5720 <nsgk5720@gmail.com>
2024-11-18 22:55:31 +0900

    -. useEffect 테스트

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom';
-import dummy from'./../db/data.json';
+import {useState, useEffect} from "react";
 
 export default function DayList(){
-    return<ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}><Link to={`/day/${day.day}`} >Day {day.day}</Link> </li>
-        ))}
-        <li></li>
-    </ul>
+    const [days, setDays] = useState([]);
+    const [count, setCount] = useState(0);
+    function onClick(){
+        setCount (count+1)
+    }
+    function onClick2(){
+        setDays([
+            ...days,
+            {
+                id:Math.random(),
+                day: 1,
+            },
+        ])
+    }
+
+    // useState처럼 계속 호출됨 그런데 2번째 인자로 배열 안에 변수명을 주면
+    // 해당 변수가 수정될 때만 호출할 수 있게됨 
+    // onClick에만 호출O // onClick2에는 호출X
+    useEffect(() =>{
+        console.log("change");
+    },[count])
+
+    return (
+        <ul className="list_day">
+            {days.map(day => (
+                <li key={day.id}><Link to={`/day/${day.day}`} >Day {day.day}</Link> </li>
+            ))}
+            <li></li>
+            <button onClick={onClick}>{count}</button>
+            <button onClick={onClick2}>day Change</button>
+        </ul>
+    )
 }
 
