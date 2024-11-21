62f8de0d0eecdbd0c1d7ec24694de88215d7b7dc
nsk5720 <nsgk5720@gmail.com>
2024-11-21 21:48:28 +0900

    -. useEffect -. fetch() 로 API호출

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,17 +1,29 @@
 import dummy from './../db/data.json'
-import { useParams } from 'react-router-dom';
+import { useParams} from 'react-router-dom';
 import Word from './Word.js'
+import {useState, useEffect} from "react";
 
 export default function Day(){
     const {day} =useParams();
-    const wordList = dummy.words.filter(word =>
-        word.day === Number(day)
-    )
+    // const wordList = dummy.words.filter(word =>
+    //     word.day === Number(day)
+    // )
+    const [words, setWords] = useState([]);
+    useEffect(() =>{
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+        });
+    },[day])
+
     return <>
     <h2>Day {day}</h2>
     <table>
         <tbody>
-            {wordList.map(word =>(
+            {words.map(word =>(
                <Word word={word} key={word.id}></Word>
             ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,37 +1,29 @@
 import { Link } from 'react-router-dom';
 import {useState, useEffect} from "react";
+import { useParams} from 'react-router-dom';
 
 export default function DayList(){
     const [days, setDays] = useState([]);
-    const [count, setCount] = useState(0);
-    function onClick(){
-        setCount (count+1)
-    }
-    function onClick2(){
-        setDays([
-            ...days,
-            {
-                id:Math.random(),
-                day: 1,
-            },
-        ])
-    }
-
     // useState처럼 계속 호출됨 그런데 2번째 인자로 배열 안에 변수명을 주면
     // 해당 변수가 수정될 때만 호출할 수 있게됨 
     // onClick에만 호출O // onClick2에는 호출X
     useEffect(() =>{
-        console.log("change");
-    },[count])
+        fetch('http://localhost:3001/days')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        });
+    },[])
 
     return (
         <ul className="list_day">
             {days.map(day => (
-                <li key={day.id}><Link to={`/day/${day.day}`} >Day {day.day}</Link> </li>
+                <li key={day.id}>
+                    <Link to={`/day/${day.day}`} >Day {day.day}</Link>
+                </li>
             ))}
-            <li></li>
-            <button onClick={onClick}>{count}</button>
-            <button onClick={onClick2}>day Change</button>
         </ul>
     )
 }
